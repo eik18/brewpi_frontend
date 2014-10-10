17d8466b2b9c97cbe5c8fe93ab71eff473c85bbc
eik18 <ekelson@gmail.com>
2014-10-09 22:40:35 -0400

    Updated with variables

--- brew.rb
+++ brew.rb
@@ -4,11 +4,23 @@ require 'json'
 require 'net/http'
 
 
+
 configure do
 	set :bind, '0.0.0.0'
 	set :port, '4567'
 end
-
+esettemp="0"
 get '/' do
-	haml :brewhome, :locals => {:temp1 => '14',:temp2=>'18'}
+	haml :brewhome, :locals => {:temp1 => '14',:temp2=>'18',:rsettemp=>esettemp}
 end
+
+get "/brewedit" do
+	haml :brewedit, :locals => {:settemp=>"0", :dutycycle=>".5",:cyclelength=>"4",:sensorselect=>"null"}
+end
+
+post "/brewpost" do
+	esettemp = params[:settemp]
+	redirect '/'
+end
+
+
